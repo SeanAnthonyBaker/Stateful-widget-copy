f1921deda877da8daa73c10f01cefbc640f58bf5
SeanAnthonyBaker <112309769+SeanAnthonyBaker@users.noreply.github.com>
2023-07-04 08:20:42 +0100

    Working call no decode

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'base-client.dart';
+// import 'dart:convert' as convert;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(), //
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   List<String> _stories = [
     'Develop a front end widget',
     'Create a ListTile view',
@@ -27,93 +38,36 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Learning Journeys'),
+        centerTitle: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Your Learning Journeys'),
-          centerTitle: true,
-        ),
-        body: Card(
-          child: Column(
-            children: _stories
-                .map((e) => ListTile(
-                      title: Text(e),
-                      tileColor: Colors.grey[100],
-                      onTap: () async {
-                        print(e);
-                        var response = await BaseClient()
-                            .get('/UXTeam')
-                            .catchError((err) {});
-                        if (response == null) {
-                          debugPrint('unsuccessful:');
-                          return;
-                        }
-                        final odata = jsonDecode(response.body);
-                        final results = odata['value'];
-                        for (var item in results) {
-                          final name = item['Lastname'];
-                          debugPrint(name.toString());
-                        }
-                      },
-                    ))
-                .toList(),
-          ),
+      body: Card(
+        child: ListView.builder(
+          itemCount: _stories.length,
+          itemBuilder: (BuildContext context, int index) {
+            String story = _stories[index];
+            return ListTile(
+                title: Text(story),
+                tileColor: Colors.grey[100],
+                onTap: () async {
+                  print(story);
+                  var response =
+                      await BaseClient().get('/UXTeam').catchError((err) {});
+                  if (response == null) {
+                    debugPrint('Unsuccessful');
+                    return;
+                  }
+
+                  //               final Map parsed = response.decode();
+                  debugPrint(response.toString());
+                  print('hi');
+                  print('there');
+                });
+          },
         ),
       ),
     );
   }
 }
-
-/*             
-              ListTile(
-                title: const Text('Develop a front end widget'),
-                tileColor: Colors.grey[100],
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Create a ListTile view'),
-                tileColor: Colors.white,
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Connect up with Cloud Fire Story'),
-                tileColor: Colors.grey[100],
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Build Master Detail navigation'),
-                tileColor: Colors.white,
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Connect up with Cloud FireStore'),
-                tileColor: Colors.grey[100],
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Develop SteamPunk OData service'),
-                tileColor: Colors.white,
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Create the ABAP communication System'),
-                tileColor: Colors.grey[100],
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Develop HTTPS access widgets'),
-                focusColor: Color(0xffecf9dd),
-                tileColor: Colors.white,
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Connect up widget with SteamPunk backend'),
-                focusColor: Color(0xffecf9dd),
-                tileColor: Colors.grey[100],
-                onTap: () {},
-              )
-   */
